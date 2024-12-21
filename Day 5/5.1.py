53f82946518ed49f550069526b5cb4fde5438cfc
DrewH711 <drex47127@gmail.com>
2024-12-20 22:52:12 -0500

    gitignore update

--- Day 5/5.1.py
+++ Day 5/5.1.py
@@ -27,7 +27,37 @@ data = """75,47,61,53,29
 61,13,29
 97,13,75,29,47"""
 
-myinput = data.splitlines()
+strinput = data.splitlines()
+finput = []
+
+for item in strinput:
+    templist=[]
+    z = item.split(",")
+    finput.append(list(map(int, z)))
 
 
+temprules = rulesdata.splitlines()
+rules = []
+for item in temprules:
+    x = item.split("|")
+    rules.append(tuple(map(int,x)))
 
+print(rules)
+pagetotal = 0
+
+for book in finput:
+    allowed = True
+    bookrules = []
+
+    for rule in rules:
+        
+        if(rule[0] in book and rule[1] in book):
+            # print(f"book: {book}")
+            # print(f"rule: {rule}")
+            if(book.index(rule[0])>book.index(rule[1])):
+                allowed = False
+    if(allowed):
+        length = len(book)
+        pagetotal+=book[int((length-1)/2)]
+
+print(pagetotal)
